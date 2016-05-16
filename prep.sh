ae6e678a54868b61976639d4fd45e1bddf275aa1
Sergey Tselovalnikov <sergeicelov@gmail.com>
2016-05-16 10:26:16 +0300

    Initial commit

---
+++ prep.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/home/serce/tmp/panama/panama/build/linux-x86_64-normal-server-release/images/jdk/
+export GRADLE_OPTS="-XaddExports:jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED"
---
+++ run.sh
@@ -0,0 +1,1 @@
+./gradlew compileJmhJava && /home/serce/tmp/panama/panama/build/linux-x86_64-normal-server-release/images/jdk/bin/java -cp build/classes/jmh/ me.serce.panex.ChecksumBenchmark
